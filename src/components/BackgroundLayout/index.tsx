f626abdc2b68da45aeb7db5839b7c9d31ef20135
Chuhj <chj9574@naver.com>
2022-10-24 23:14:37 +0900

    Add a background layout

---
+++ src/components/BackgroundLayout/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from './index.module.scss';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+function BackgroundLayout({ children }: LayoutProps) {
+  return (
+    <div className={styles.container}>
+      <Image src="/images/background.png" alt="background" layout="fill" objectFit="cover" />
+      <div className={styles.innerBox}>
+        <div className={styles.children}>{children}</div>
+        <Image src="/images/inner-box.png" alt="inner-box" layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
+}
+
+export default BackgroundLayout;

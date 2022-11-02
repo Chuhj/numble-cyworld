b0f74e0682f92d42c96611f9ced700725f26359e
Chuhj <chj9574@naver.com>
2022-11-02 23:39:12 +0900

    Fetch 4 diaries to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
+import { gql } from '@apollo/client';
+import client from '../src/apollo-client';
 import Home from '../src/components/Home';
-import type { NextPage } from 'next';
 
-const HomePage: NextPage = () => {
-  return <Home />;
-};
+export interface HomeDiariesData {
+  diaries: { number: number; title: string }[];
+}
+
+const GET_HOME_DIARIES = gql`
+  query FetchBoards($page: Int = 1) {
+    diaries: fetchBoards(page: $page) {
+      number
+      title
+    }
+  }
+`;
+
+function HomePage({ diaries }: HomeDiariesData) {
+  return <Home diaries={diaries} />;
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query<HomeDiariesData>({
+    query: GET_HOME_DIARIES,
+  });
+
+  return {
+    props: {
+      diaries: data.diaries.slice(0, 4),
+    },
+  };
+}
 
 export default HomePage;

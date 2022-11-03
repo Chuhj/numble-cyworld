a28a4589e3c61a2a11c11b3d969ba3d4a8e745a0
Chuhj <chj9574@naver.com>
2022-11-03 22:32:10 +0900

    Setting ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import AppLayout from '../src/components/AppLayout';
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import client from '../src/apollo-client';
+import AppLayout from '../src/components/AppLayout';
 import 'styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <ApolloProvider client={client}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ApolloProvider>
   );
 }
 
